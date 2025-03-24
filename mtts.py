958c8b517a2b5eda8065d37db1c88bc9a0180927
edge <dcc@monika.love>
2025-03-24 15:10:55 +0800

    init

---
+++ mtts.py
@@ -0,0 +1,93 @@
+import datetime
+import os
+import zipfile
+from io import BytesIO
+import shutil
+import requests
+import time
+
+timestamp = f'{time.time():.21f}'
+
+
+
+CHATTTS_URL = f"http://127.0.0.1:8000/generate_voice"
+
+
+# main infer params
+body = {
+    "text": [
+        "我今天过得还不错[uv_break], 你呢?[lbreak]",
+    ],
+    "stream": False,
+    "lang": None,
+    "skip_refine_text": True,
+    "refine_text_only": False,
+    "use_decoder": True,
+    "audio_seed": 2,
+    "text_seed": 42,
+    "do_text_normalization": True,
+    "do_homophone_replacement": False,
+}
+
+# refine text params
+params_refine_text = {
+    "prompt": "",
+    "top_P": 0.7,
+    "top_K": 20,
+    "temperature": 0.7,
+    "repetition_penalty": 1,
+    "max_new_token": 384,
+    "min_new_token": 0,
+    "show_tqdm": True,
+    "ensure_non_empty": True,
+    "stream_batch": 24,
+}
+body["params_refine_text"] = params_refine_text
+
+# infer code params
+params_infer_code = {
+    "prompt": "[speed_5]",
+    "top_P": 0.1,
+    "top_K": 20,
+    "temperature": 0.3,
+    "repetition_penalty": 1.05,
+    "max_new_token": 2048,
+    "min_new_token": 0,
+    "show_tqdm": True,
+    "ensure_non_empty": True,
+    "stream_batch": True,
+    "spk_emb": "蘁淰敕欀摃誌緘義囡胹讵祪萀梂晳癧亇婇嚕儝揇偩贻咆煴淀蠀欐萵貺箚弃胦菍弁夞皥焆喦卢狧乩夏淔莨臃赽奛溕筡誑緶貿捨讖卢瑫嬅哙硚惣蚵刻玏炉跸徱澾登嬖絢烇嫷媓蔢产虜椪眕俟徊吞詸愣備恍珳湉璑訷珽菹訴痙濽圴謗瘾皡憖啤囊偐惏嶩役磅惃碝贬貇行楝薇磉数綊蟊弤夋荄壪攫撧杶岈硯葳赛悫宸岩稼琜串汏僎灡峂蝇筋茹聈柵焵皿綏缊橥爝澺縬樢訣潙许壚朔仑螽穨糼稰礌漖噍脠庭穪栽嚽袿蟢朁睬筸獸蜍荃俜椉狴掠歾泓葁潚蚗刣悬縶執萏淪肬涼覎培煟苇攁蕘瞥覹緌玽忖熒苼偶巴氶壡卝僕聥栘袴瞗匥弯剫堎搒烅芡渢蒺仉濃猿焳觔吼嚾簬伋諿圀晑牣缄澜枡溒甆欌槙螶璭惝賙扣氒嘕質僜乧畭徉蟖裔既流橊卺奪襾耨嬖脡甆槡巢誸倦訐忂匼俵宰凥覡穰捠斋孖瀤謹讗揲害祩歊蠯旸忎継亍憭徿礯蜷絕凵腂凾疼渴痳旑賧槢浃圕畧晖庞捻翺岊澛縃婳哵喳唗趢咊綼倅佹艅丽趔攪懦蟜牢庨蒘薪蜩煐揈羄获话涴婔傊庪蚫曃氻肙瞥响丹粫璯蕷舺捆搞爳瞻僱潜袄恛懝嗀碥嶎椓一奥濇嵊卂燡懼礅護懭爋蚿檠蟔氖謻淫曇乯槙孓僷疶笺慛誏籜扰固嚲幦吲朸罺眅晝噱簭椼嘎坷嬢粆师恢埨伮跭侂庒瞭幕擛裌藩屙径皎蕾猨徲徎俬渰畣瓂嵭璌砟勗睃沭吾嗅端匈椃棒瓁刉觤伎虗貉柨燜緷奦曛綡拷撮箓縳蠺綢臑栳愆蛴聱嫼亞人翢疋貼横査艼妽菪梷薓棆焉彘撙蝳籯嬎谡毮牥狊垦岩刡趄虾葤纵爩媳泟惏撙剗瓕濂届竨跘匊殱幓你侜羯籕匐璾凡樃俋臺虘蝄懇罶悥孆击捪蛖畋屁蠐蟦埙夬俟抗籵惉柌箼瞀庻勨串捅窮氶賰燧捵蕓汐藈噱臷児汱留翷枾昅想慱羆蚅聢珹礦諅坔嚇缤冫窙蟓壡洦啓茖汬嶉賭汯紡屒揁熀蛾数篧哞撌塔妥蓗懘犌富圃胃莧絗喘葔改脧焛摆儭庥挖謪擾緖蓐卼褟萎磗侻恏嫒愗欮樞羻喻厚欫参姿剝堬絊挒暘擋緷貧妖欷牶诬囌揋膝湷觸柗灚烚誵暡讟卒縉乍跊疥褧皏菈吓穭脓呲挿燐藒澬珹嗧茪芝灲吋崩请瀓蜋棦掙沝刴彸褕缥誐喘胤櫂愄娇肥吥匚佯揔舔瑪燣孲珬谱炆夤梑狕祠痸浾薐萂暟葯俴涊怰蕲眞煍嘷趌褖弹硒囑琋焧截嵨蘈卥呬畸痾厾橓槔赒熰毪稵囨瀺綰穧楳囹籽窷俆坵萵澳瘏穉焬睳洲蓴懬膄揳妦悰尯堇翩葾弉忲昦蟝慎摏衃榶硟兡啥焛堵汼殗搩枌狎斳蒞貼敱叏刳梋莯椥刣吿埓仹熖悲嫿嫤哆怔祸嵢狴斻肎唤樵糪禾瓺摏璂跨卶欢刖薁嬼蚨壳栮余育熪跭讘勖亾擕硬悦痕屺櫞袁椤穟帀㴃",
+    "manual_seed": 2,
+}
+body["params_infer_code"] = params_infer_code
+
+
+try:
+    response = requests.post(CHATTTS_URL, json=body)
+    response.raise_for_status()
+    with zipfile.ZipFile(BytesIO(response.content), "r") as zip_ref:
+        # save files for each request in a different folder
+        dt = datetime.datetime.now()
+        tgt = f"./temp/{timestamp}/"
+        os.makedirs(tgt, 0o755)
+        zip_ref.extractall(tgt)
+        #print("Extracted files into", tgt)
+
+except requests.exceptions.RequestException as e:
+    print(f"Request Error: {e}")
+
+
+
+
+url = "http://127.0.0.1:6842/change_voice"
+trg_file = open(f"temp/{timestamp}/res.wav", "rb")
+data = {}
+files = {"sample": trg_file}
+
+r = requests.post(url=url, data=data, files=files)
+
+c = r.content
+with open(f"result/{timestamp}.wav", "wb+") as res_f:
+    res_f.write(c)
+
+#shutil.rmtree(f"temp/{timestamp}/")
